5c2b80ec6b891a51cfff7c7cc2c93ff60c7b979e
admin <admin@MacOs-cua-Trung.local>
2025-06-23 10:10:16 +0700

    feat: add p tag in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Page() {
   return (
     <>
-      <h1>Welcome to My Profile</h1>
+      <h1>Welcome to My Profile.</h1>
       <p>Hello, My name is Trung.</p>
       <p>Im from DaNang city.</p>
       <p>This is deployment using CD github action page.</p>
@@ -13,7 +13,6 @@ export default function Page() {
         width={100}
         height={100}
       />
-      ;
     </>
   );
 }
